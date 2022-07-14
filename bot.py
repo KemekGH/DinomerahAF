ab4c0af967d6e495f012dba132af9832b28e5e7a
KemekGH <109185508+KemekGH@users.noreply.github.com>
2022-07-14 08:49:05 +0700

    Double Forcesubs

--- bot.py
+++ bot.py
@@ -48,7 +48,7 @@ class Bot(Client):
             except Exception as a:
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
-                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_GROUP value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL}")
+                self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_GROUP value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_GROUP}")
                 self.LOGGER(__name__).info("\nBot Stopped. Join https://t.me/Powdrs for support")
                 sys.exit()
         try:
